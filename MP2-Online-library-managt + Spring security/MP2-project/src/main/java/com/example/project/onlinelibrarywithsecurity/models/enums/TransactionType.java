1ee97bf1ecfdac9bf35a358cd81848c71aa6f17a
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2024-05-25 17:31:26 +0530

    readme

--- MP2-Online-library-managt + Spring security/MP2-project/src/main/java/com/example/project/onlinelibrarywithsecurity/models/enums/TransactionType.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.onlinelibrarywithsecurity.models.enums;
-
-public enum TransactionType {
-
-    ISSUE,
-
-    RETURN
-}
--- MP2-Online-library-managt + Spring security/MP2-project/src/main/java/com/example/project/onlinelibrarywithsecurity/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.onlinelibrarywithsecurity.repository;
-
-import com.example.project.onlinelibrarywithsecurity.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-}
